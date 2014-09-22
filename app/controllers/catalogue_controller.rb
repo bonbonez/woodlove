3eb9a32a34eacbbca6850ee23730e71ecf7a0fac
Artem Petrov <bonbonez@gmail.com>
2014-09-22 23:44:46 +0400

    landing improvements

--- app/controllers/catalogue_controller.rb
+++ app/controllers/catalogue_controller.rb
@@ -2,6 +2,7 @@ class CatalogueController < ApplicationController
 
   def index
     @item = Item.first
+    @items_for_popular = [Item.first, Item.first, Item.first, Item.first, Item.first, Item.first, Item.first, Item.first, Item.first]
   end
 
 end
